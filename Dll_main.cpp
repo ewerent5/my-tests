7f7b0bfce0cf8e01e05e1f973f7cfc0bc78770d0
Alex <razor3726@gmail.com>
2025-09-17 14:21:31 +0300

    Update Dll_main.cpp
    injector for embedding a DLL in a process

--- Dll_main.cpp
+++ Dll_main.cpp
@@ -1,10 +1,14 @@
-// Dll для внидрения в процессы | Dll for implementation in processes
+// Dll Г¤Г«Гї ГўГ­ГЁГ¤Г°ГҐГ­ГЁГї Гў ГЇГ°Г®Г¶ГҐГ±Г±Г» | Dll for implementation in processes
 
+/*
 
+My injector
+
+*/
 #include <Windows.h>
 #include <iostream>
  
-BOOL WINAPI  point(HMODULE hm){ //<-- точка запуска | start point
+BOOL WINAPI  point(HMODULE hm){ //<-- ГІГ®Г·ГЄГ  Г§Г ГЇГіГ±ГЄГ  | start point
 	HANDLE process = OpenProcess(PROCESS_ALL_ACCESS, false, GetCurrentProcessId()); // <--- process id
 
 	char x1 = 254;
@@ -14,8 +18,8 @@ BOOL WINAPI  point(HMODULE hm){ //<-- точка запуска | start point
 
 	else {
 		Sleep(50);
-		while (true) {							//  			   |ячейка | cell
-												//				   \/			  \/значение | value
+		while (true) {							//  			   |ГїГ·ГҐГ©ГЄГ  | cell
+												//				   \/			  \/Г§Г­Г Г·ГҐГ­ГЁГҐ | value
 			int memoryWindows = WriteProcessMemory(process, (LPVOID)0x128212550, &x1, (DWORD)sizeof(x1), NULL);
 			//int memoryWindows2 = WriteProcessMemory(process, (LPVOID)0x0000022F5784EEE0, &value2, (DWORD)sizeof(value), NULL);
 			//int memoryWindows3 = WriteProcessMemory(process, (LPVOID)0x000000E6C0D5F6E0, &value3, (DWORD)sizeof(value), NULL);
